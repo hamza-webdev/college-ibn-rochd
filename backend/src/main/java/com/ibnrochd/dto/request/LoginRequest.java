05be3f1bec408cf693d9b41a403574ed14b565e8
hamza-webdev <bh.bedwi@gmail.com>
2025-06-01 17:54:15 +0200

    Ajout des annotations @NoArgsConstructor et @AllArgsConstructor dans les classes LoginRequest et SignupRequest pour faciliter la création d'instances.

--- backend/src/main/java/com/ibnrochd/dto/request/LoginRequest.java
+++ backend/src/main/java/com/ibnrochd/dto/request/LoginRequest.java
@@ -2,12 +2,15 @@ package com.ibnrochd.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank
-    private String email; // Assurez-vous que ce nom correspond à getEmail()
-
+    private String email;
     @NotBlank
-    private String motDePasse; // Assurez-vous que ce nom correspond à getMotDePasse()
+    private String motDePasse;
 }
--- backend/src/main/java/com/ibnrochd/dto/request/SignupRequest.java
+++ backend/src/main/java/com/ibnrochd/dto/request/SignupRequest.java
@@ -4,27 +4,27 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequest {
     @NotBlank
     @Size(min = 2, max = 50)
-    private String prenom; // Correspond à getPrenom()
-
+    private String prenom;
     @NotBlank
     @Size(min = 2, max = 50)
-    private String nom; // Correspond à getNom()
-
+    private String nom;
     @NotBlank
     @Size(max = 50)
     @Email
-    private String email; // Correspond à getEmail()
-
-    private Set<String> role; // Correspond à getRole()
-
+    private String email;
+    private Set<String> role;
     @NotBlank
     @Size(min = 6, max = 40)
-    private String motDePasse; // Correspond à getMotDePasse()
+    private String motDePasse;
 }
